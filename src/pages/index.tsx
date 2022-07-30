be64890968fce07a664aa2710345d60f3638452d
Angel decode <miguelangelpacompia@gmail.com>
2022-07-30 14:55:49 -0500

    mobile version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,52 +42,55 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="card flex min-h-screen justify-center items-center bg-slate-500 text-blue-500">
-        <div className="w-1/3 min-w-[550px] bg-white rounded-lg overflow-hidden p-6 my-28">
-          <div onClick={() => setCardRotate(!cardRotate)} className="cursor-pointer">
+      <div className="card flex min-h-screen justify-center md:items-center bg-slate-500 text-blue-500">
+        <div className="w-full md:max-w-md bg-white md:rounded-lg overflow-hidden pt-6 sm:p-6 md:my-28">
+          <div
+            onClick={() => setCardRotate(!cardRotate)}
+            className="cursor-pointer w-[325px] h-[210px] mx-auto md:h-[250px] md:w-[400px]"
+          >
             <div
-              className={`card-container select-none w-full text-white
-              ${cardRotate && "active"} max-w-[470px] relative z-20 mx-auto`}
+              className={`card-container select-none text-white
+              ${cardRotate && "active"} relative z-20 mx-auto`}
             >
               {/* FRONT */}
               <div
                 className={`card-bank overflow-hidden p-5 card-bank-front rounded-lg bg-cover bg-center
-                min-h-[260px] shadow-[0_35px_10px_-15px_rgba(0,0,0,0.3)] ${bgImage()}`}
+                shadow-[0_35px_10px_-15px_rgba(0,0,0,0.3)] ${bgImage()} text-sm h-[210px] md:h-[250px]`}
               >
-                <div className='h-[80px]'>
+                <div className='min-h-[50px] md:h-[80px]'>
                   {imgLogo === "4" && 
-                    <div className="ml-auto w-32">
+                    <div className="ml-auto w-20 md:w-32">
                       <Image src={visa} alt="visa logo" className='object-cover' />
                     </div>
                   }
                   {imgLogo === "5" && 
-                    <div className="ml-auto w-32">
+                    <div className="ml-auto w-20 md:w-32">
                       <Image src={masterCard} alt="mastercard logo" className='object-cover' />
                     </div>
                   }
                   {imgLogo === "3" && 
-                    <div className="ml-auto w-32">
+                    <div className="ml-auto w-20 md:w-32">
                       <Image src={americanEx} alt="american express logo" className='object-cover' />
                     </div>
                   }
-                  {!imgLogo && <div className="ml-auto rounded bg-gray-600 w-24 h-12"></div>}
+                  {!imgLogo && <div className="ml-auto rounded bg-gray-600 w-24 h-10 md:h-12"></div>}
                 </div>
-                <div>
-                  <Image src={chip} alt="chip" width={50} height={40} />
+                <div className='w-[40px]'>
+                  <Image src={chip} alt="chip"/>
                 </div>
                 <div className="">
-                  <div className="drop-shadow-[0_35px_35px_rgba(255,255,255,1)]">
-                    {/* <p className="text-sm text-[#7d8994]">Card Number</p> */}
-                    <p className="text-2xl">{cardNum || "#### #### #### ####"}</p>
+                  <div className="drop-shadow-[1px_1px_4px_rgba(20,10,10,1)]">
+                    {/* <p className="text-sm text-">Card Number</p> */}
+                    <p className="md:text-2xl">{cardNum || "#### #### #### ####"}</p>
                   </div>
 
-                  <div className="flex justify-between mt-5">
+                  <div className="flex justify-between mt-4">
                     <div className="">
-                      <p className="text-sm text-[#7d8994]">Name Card</p>
+                      <p className="text-sm text-gray-700">Name</p>
                       <div className="name uppercase">{cardName || "Jhon Doe"}</div>
                     </div>
                     <div className="">
-                      <p className="text-sm text-[#7d8994]">Expiration</p>
+                      <p className="text-sm text-gray-700">Expiration</p>
                       <p className="uppercase">
                         {cardExpiration || "MM/YY"}
                       </p>
@@ -99,33 +102,32 @@ const Home: NextPage = () => {
               {/* BACK */}
               <div
                 className={`card-bank p-5 card-bank-back absolute top-0 left-0 w-full rounded-lg
-                overflow-hidden bg-cover min-h-[287px] shadow-[0_35px_10px_-15px_rgba(0,0,0,0.3)]
-                ${bgImage()}`}
+                overflow-hidden bg-cover shadow-[0_35px_10px_-15px_rgba(0,0,0,0.3)]
+                ${bgImage()} h-[210px] md:h-[250px]`}
               >
-                <div className='w-full h-12 bg-black absolute left-0 right-0'>
+                <div className='w-full h-10 md:h-12 bg-black absolute left-0 right-0'>
                 </div>
                 <div className='flex mt-14 items-center'>
                   <div className="flex flex-col w-8/12">
-                    <small className='w-20 h-4 rounded bg-gray-600 my-1'></small>
-                    {/* <small className=''>Firma Autorizada</small> */}
-                    <div className="info w-full h-8"></div>
+                    <small className='w-20 h-3 md:h-4 rounded bg-gray-600 my-1'></small>
+                    <div className="info w-full h-6 md:h-8"></div>
                   </div>
                   <div className="flex flex-col">
                     <small>CCV</small>
-                    <div className="bg-white h-7 text-gray-400 px-2 italic">{cardCCV || "ccv"}</div>
+                    <div className="bg-white h-5 md:h-7 text-gray-400 px-2 italic">{cardCCV || "ccv"}</div>
                   </div>
                 </div>
                 <div className='mt-6'>
-                  <small className='w-32 h-4 rounded bg-gray-600 my-1 block'></small>
-                  <small className='w-44 h-4 rounded bg-gray-600 my-1 block'></small>
-                  <small className='w-32 h-4 rounded bg-gray-600 my-1 block'></small>
+                  <small className='w-32 h-3 md:h-4 rounded bg-gray-600 my-1 block'></small>
+                  <small className='w-44 h-3 md:h-4 rounded bg-gray-600 my-1 block'></small>
+                  <small className='w-32 h-3 md:h-4 rounded bg-gray-600 my-1 block'></small>
                 </div>
               </div>
             
             </div>
           </div>
 
-          <form ref={formRef} className="flex flex-col gap-3 mt-5 text-gray-800" onSubmit={handleSubmit}>
+          <form ref={formRef} className="flex flex-col gap-3 mt-5 text-gray-800 max-w-[500px] mx-auto px-4 md:px-0" onSubmit={handleSubmit}>
             <input
               type="text"
               value={cardNum}
